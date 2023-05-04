c19a0e70e2645b67c25b91b640b295610a422b47
vsiv16 <55516887+vsiv16@users.noreply.github.com>
2023-05-03 23:01:58 -0700

    cleanup + readme

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# Experiment Manager Decorator
+# Experiment Manager
+
+The Experiment Manager is a Python decorator to help automate and manage running experiments, especially for data science and in notebook development environments.
+
+## Setup
+
+1. Download `expt_mgr.py` from this [GitHub Gist](https://gist.github.com/vsiv16/b42431beff8c76a79046ee7162d6f6e6)
+
+2. Move `expt_mgr.py` into your project's directory (wherever the experiment code you would like to automate is!) 
+
+## Usage
+
+1. Import the `experiment_entry` decorator as follows:
+    ```
+    from expt_mgr import experiment_entry
+    ```
+
+2. Annotate the function you would like to automate as an experiment with `@experiment-entry` in order to apply the decorator to it. 
+
+3. Create lists of any parameter combinations you wish to try out across experiment trials, and supply them to the decorator as keyword arguments. Experiment Manager will automatically run all parameter combinations for you!
+
+    *Note:* Make sure to keep the naming of the keyword arguments consistent with the original parameter names in the function!
+
+4. Finally, simply call the function *(without any parameters)*.
+
+### Example Usage:
+
+```
+    # parameters
+    p1_list = [1, 2, 3]
+    p2_list = [1]
+    p3_list = [3, 4]
+
+    # model1() function annotated with @experiment_entry decorator
+    @experiment_entry(p1=p1_list, p2=p2_list, p3=p3_list)
+    def model1(p1, p2, p3):
+        return p1 + p2 + p3
+
+    model1()
+```
+
+## What to Expect
+
+### Outputs
+
+Upon running the experiment code/cell, you should see an output summary table containing the experiment trial number, the parameter values utilized, and the function/model output.
+
+#### Example Output Summary Table:
+<img src="images/output-tbl-example.png" width="400">
+
+### Logging
+
+- All experiment logs are saved by Experiment Manager in a dedicated folder within your project called `.log_records`
+- For every run of a decorated experiment function, Experiment Manager saves a timestamped folder (named like `run_<date>_<time>`) within `log_records` containing:
+
+1. A copy of your project source code at the time of running
+
+2. The experiment's output summary table as a csv file (named `<function>.csv`)
+
+#### Example Logging Directory Structure:
+```
+.log_records
+├── run_2023-05-03_22:24:15
+│   ├── my-project
+│   │   ├── my_model.py
+│   └── model1_out.csv
+└── run_2023-05-03_22:25:05
+    ├── my-project
+    │   ├── my_model.py
+    └── model1_out.csv
+```
--- expt_mgr.py
+++ expt_mgr.py
@@ -8,7 +8,6 @@ def experiment_entry(**params):
     def decorator(func):
         def wrapper(*args, **kwargs): # args, kwargs empty initially
 
-            # TODO: create new output folder -- named "Run_timestamp"
             # Get the current working directory path
             cwd = os.getcwd()
             log_records_path = os.path.expanduser(cwd + "/.log_records")
@@ -23,60 +22,40 @@ def experiment_entry(**params):
             if not os.path.exists(current_saving_folder):
                 os.makedirs(current_saving_folder)
 
-            # TODO: save a copy of user's code in output folder -- named "Version_timestamp??"
-            # Go through all files in the current working directory
+            # Go through all files in the current working directory & save
             save_all_files(cwd, current_saving_folder)
 
-            #######################################################################
-            # print("PARAMS: ", params) # dictionary (param name: param value)
-            # print("*PARAMS: ", *params) # only key names
-            # print("PARAMS KEYS: ", list(params.keys()))
             param_names = list(params.keys())
-
-            # prep list of value lists for itertools product
-            # ex for params: {'p1': [1, 2, 3], 'p2': [1, 2], 'p3': [1, 2, 3, 4]}
-            # vals_list is [[1, 2, 3], [1, 2], [1, 2, 3, 4]]
+            # prep list of param value lists (for itertools product)
             vals_list = []
             for key in params:
                 vals_list.append(params[key])
-            # print("VALS_LIST: ", vals_list)
 
-            # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
+            # create experiment summary dataframe
             column_names = param_names + ["Model Output"]
             df = pd.DataFrame(columns=column_names)
-            # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
 
             # run all experiments: iterate through all possible parameter value combinations
             for values in itertools.product(*vals_list):
-                expt_info = []
-                # print("VALUES PRODUCT: ", values)
-                
+                expt_info = [] # save params/outputs for summary dataframe
                 # prep params for each experiment 
                 for i, value in enumerate(values):
                     param_name = param_names[i]
                     kwargs[param_name] = value
-                    # $$$$$$$$$$$$$$$$$$$$$ #
                     expt_info.append(value)
-                    # $$$$$$$$$$$$$$$$$$$$$ #
 
                 # run experiment (call func for each combination of param values)
                 output = func(*args, **kwargs)
-                # print(output)
-                # $$$$$$$$$$$$$$$$$$$$$ #
                 expt_info.append(output)
-                # $$$$$$$$$$$$$$$$$$$$$ #
                 df.loc[len(df.index)] = expt_info
-            #######################################################################
 
-            # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
-            df.index = ["Experiment " + str(i+1) + ":" for i in range(len(df.index))]
-            styled_df = df.style.format({'Model Output': lambda x: f'<span style="background-color: yellow">{x}</span>'})
-            # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
+            df.index = ["Experiment " + str(i+1) for i in range(len(df.index))]
+            # styled_df = df.style.format({'Model Output': lambda x: f'<span style="background-color: yellow">{x}</span>'})
 
-            # TODO: download csv & save into output folder
+            # download csv & save into output folder
             save_output(df, current_saving_folder, func)
 
-            return styled_df
+            return df # styled_df
 
         return wrapper
     return decorator
@@ -85,7 +64,6 @@ def experiment_entry(**params):
 def save_all_files(cwd, current_saving_folder):
     current_saving_folder=os.path.join(current_saving_folder, os.path.basename(cwd))
     for filename in os.listdir(cwd):
-        # print(filename)
         if filename[0] == '.':
             continue
         f = os.path.join(cwd, filename)
@@ -98,7 +76,6 @@ def save_all_files(cwd, current_saving_folder):
 
 
 def save_file(filename, cwd, current_saving_folder):
-    # print("File to be serialised ", filename)
     # open the file for reading
     source_file = os.path.join(cwd, filename)
 
@@ -118,8 +95,4 @@ def save_output(df, current_saving_folder, func):
     func_name = func_string[first_space+1:second_space]
 
     output_file_path = os.path.expanduser(current_saving_folder + "/" + func_name + "_out.csv")
-    df.to_csv(output_file_path) 
-
-
-
-
+    df.to_csv(output_file_path)
